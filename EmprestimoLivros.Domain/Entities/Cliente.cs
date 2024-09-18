fcf78868f35012938ccfe2c17ae5144b6d0b5610
Green Sistemas <greensistemasdev@outlook.com>
2024-09-17 22:44:43 -0400

    Trabalhando com clean architecture

---
+++ EmprestimoLivros.Domain/Entities/Cliente.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmprestimoLivros.Domain.Entities
+{
+    public class Cliente
+    {
+        public int Id { get; private set; }
+        public string CliCpf { get; private set; }
+        public string CliNome { get; private set; }
+        public string CliEndereco { get; private set; }
+        public string CliCidade { get; private set; }
+        public string CliBairro { get; private set; }
+        public string CliNumero { get; private set; }
+        public string CliTelefoneCelular { get; private set; }
+        public string ClitelefoneFixo { get; private set; }
+
+        public Cliente(int id, string clicpf, string clinome, string cliendereco,
+            string clicidade, string clibairro, string clinumero, string clitelefonecelular,
+            string clitelefonefixo)
+        {
+            Id = id;
+            ValidationDomain(clicpf, clinome, cliendereco, clicidade, clibairro, clinumero, clitelefonecelular, clitelefonefixo);
+        }
+
+        public void ValidationDomain(string clicpf, string clinome, string cliendereco,
+            string clicidade, string clibairro, string clinumero, string clitelefonecelular,
+            string clitelefonefixo)
+        {
+            CliCpf = clicpf;
+            CliNome = clinome;
+            CliEndereco = cliendereco;
+            CliCidade = clicidade;
+            CliBairro = clibairro;
+            CliNumero = clinumero;
+            CliTelefoneCelular = clitelefonecelular;
+            ClitelefoneFixo = clitelefonefixo;
+        }
+    }
+}
+
